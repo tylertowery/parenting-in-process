2beaff0abad4d7208e37c44d0393a3e4ed2825c8
Tyler Towery <tylertowery21@gmail.com>
2023-08-02 16:49:49 -0700

    adding some styling to better align elements

--- src/app/components/GuidingPrinciples.tsx
+++ src/app/components/GuidingPrinciples.tsx
@@ -7,19 +7,19 @@ const GuidingPrinciples = ({ }: GuidingPrinciplesProps) => {
   return (
     <div>
       <div>
-        <h1>Guiding Principles Section</h1>
+        <h1 className="section-header">Guiding Principles Section</h1>
       </div>
-      <div>
-        <div>
-          <h3>Connection</h3>
+      <div className="principles">
+        <div className="principle">
+          <h3 className="section-header">Connection</h3>
           <p>Parenting is more than just a set of skills. It is, first and foremost, a relationship. Parenting through connection is about deepening the quality of relationship between parent and child, as both learn and grow as individuals and a family. Keeping connection at the heart of parenting is key.</p>
         </div>
-        <div>
-          <h3>&quot;Good Enough&quot; Parenting</h3>
+        <div className="principle">
+          <h3 className="section-header">&quot;Good Enough&quot; Parenting</h3>
           <p>Parents aren&apos;t meant to get it perfect and striving for perfection gets in the way of being the parents that children need. There is power in repair and with a willingness to learn, grow, and keep showing up, parents can “be” who their children need: perfectly imperfect and human. Good enough really is good enough.</p>
         </div>
-        <div>
-          <h3>Reflection</h3>
+        <div className="principle">
+          <h3 className="section-header">Reflection</h3>
           <p>Reflection gives way to empowered parenting. With reflection, parents can bring conscious awareness to their parenting, and explore what barriers might be preventing them from meeting their child&apos;s needs and making the changes they desire. Reflection brings awareness, and with awareness you have choice. </p>
         </div>
       </div>
